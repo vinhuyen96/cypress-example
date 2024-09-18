d43fc4319a844bcc3ef010d0fc4ce6f8f25612b3
vinhuyen96 <baoanhanh961@gmail.com>
2024-09-18 22:28:17 +0700

    OOP common

---
+++ cypress/support/pages/BasePage.js
@@ -0,0 +1,20 @@
+class BasePage {
+
+	navigate(url) {
+		cy.visit(url);
+	}
+
+	clickElement(selector) {
+		cy.get(selector).click();
+	}
+
+	typeInInput(selector, text) {
+		cy.get(selector).type(text);
+	}
+
+	verifyTextInBody(text) {
+		cy.get('body').should('contain', text);
+	}
+}
+
+export default BasePage;

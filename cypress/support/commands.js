35c97bf0fcdf58a72d272212109db7efaf816aba
vinhuyen96 <baoanhanh961@gmail.com>
2024-09-19 15:44:08 +0700

    Define login test as a common

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,13 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('login', () => {
+	//read file account.json
+	cy.fixture('account.json').then((account) => {
+		//navigate
+		cy.visit(Cypress.config().baseUrl)
+		//input account
+		cy.get('input[name="uid"]').type(account.userId)
+		cy.get('input[name="password"]').type(account.password)
+		//submit
+		cy.get('input[name="btnLogin"]').click()
+	});
+	cy.contains('Manger Id :').should('be.visible')
+})
